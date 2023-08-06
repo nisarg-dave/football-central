cab1d22ae13d593d3e6bd061a03036c825ea196e
Nisarg Dave <50187169+nisarg-dave@users.noreply.github.com>
2023-08-06 16:10:23 +0800

    Feature/fcbks blog pagination (#28)
    * feat:Created pagination component and able to dynamically render them
    
    * feat: Able to restrict 12 articles per page

--- pages/laLiga/fcbks-blog/index.tsx
+++ pages/laLiga/fcbks-blog/index.tsx
@@ -1,6 +1,6 @@
 import { GetServerSideProps } from "next";
 import Link from "next/link";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useMemo } from "react";
 import Default from "../../../layouts/Default";
 import { articles, Tab } from "../../../typings";
 import Tabs from "../../../components/widgets/Tabs";
@@ -13,6 +13,9 @@ interface fCBkHomeProps {
 
 function Index({ articles }: fCBkHomeProps) {
   const [displayedArticles, setDisplayedArticles] = useState<articles[]>([]);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [startIndex, setStartIndex] = useState(0);
+  const [endIndex, setEndIndex] = useState(12);
   const tabs = useTabs();
   const setActiveTab = useSetActiveTab();
 
@@ -35,11 +38,25 @@ function Index({ articles }: fCBkHomeProps) {
       const filteredArticles = articles.filter(
         (article) => article.categoryName === selectedTab.name
       );
+      setCurrentPage(0);
+      setStartIndex(0);
+      setEndIndex(12);
       setDisplayedArticles(filteredArticles);
     } else {
+      setCurrentPage(0);
+      setStartIndex(0);
+      setEndIndex(12);
       setDisplayedArticles(articles);
     }
   };
+
+  const numberOfPages = useMemo(() => {
+    const pages: number[] = [];
+    for (let i = 0; i < Math.ceil(displayedArticles.length / 12); i++) {
+      pages.push(i);
+    }
+    return pages;
+  }, [displayedArticles]);
 
   return (
     <Default>
@@ -50,7 +67,7 @@ function Index({ articles }: fCBkHomeProps) {
         </h2>
         <Tabs tabs={tabs} onClick={handleSelectedTab} />
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-3 md:gap-6 p-2 md:p-6">
-          {displayedArticles.map((article) => (
+          {displayedArticles.slice(startIndex, endIndex).map((article) => (
             <Link key={article._id} href={`/laLiga/fcbks-blog/${article.slug}`}>
               <div className="border border-transparent rounded-lg group cursor-pointer overflow-hidden">
                 <img
@@ -65,6 +82,67 @@ function Index({ articles }: fCBkHomeProps) {
             </Link>
           ))}
         </div>
+        {numberOfPages.length > 1 ? (
+          <div className="py-2 px-4 flex justify-end w-full">
+            <nav>
+              <ul className="flex">
+                <li>
+                  {currentPage !== 0 ? (
+                    <a
+                      className="cursor-pointer relative block rounded-full bg-transparent px-3 py-1.5 text-sm text-black transition-all duration-300 hover:bg-black hover:text-yellow-300"
+                      onClick={() => {
+                        const previousPage = currentPage - 1;
+                        if (previousPage >= 0) {
+                          setCurrentPage(previousPage);
+                          setStartIndex(startIndex - 12);
+                          setEndIndex(endIndex - 12);
+                        }
+                      }}
+                    >
+                      Previous
+                    </a>
+                  ) : (
+                    <></>
+                  )}
+                </li>
+                {numberOfPages.map((page, index) => (
+                  <li key={index}>
+                    {index === currentPage ? (
+                      <a className="relative block rounded-full px-3 py-1.5 text-sm transition-all duration-300 bg-black text-yellow-300">
+                        {index}
+                      </a>
+                    ) : (
+                      <a className="relative block rounded-full bg-transparent px-3 py-1.5 text-sm text-black transition-all duration-300">
+                        {index}
+                      </a>
+                    )}
+                  </li>
+                ))}
+                <li>
+                  {currentPage !== numberOfPages.length - 1 ? (
+                    <a
+                      className="cursor-pointer relative block rounded-full bg-transparent px-3 py-1.5 text-sm text-black transition-all duration-300 hover:bg-black hover:text-yellow-300"
+                      onClick={() => {
+                        const nextPage = currentPage + 1;
+                        if (nextPage < numberOfPages.length) {
+                          setCurrentPage(nextPage);
+                          setStartIndex(startIndex + 12);
+                          setEndIndex(endIndex + 12);
+                        }
+                      }}
+                    >
+                      Next
+                    </a>
+                  ) : (
+                    <></>
+                  )}
+                </li>
+              </ul>
+            </nav>
+          </div>
+        ) : (
+          <></>
+        )}
       </div>
     </Default>
   );
