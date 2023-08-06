fcbe8ae343dcc5bf830ab3755c88d022fce9ee0b
Nisarg Dave <ndave630@gmail.com>
2023-08-06 16:06:15 +0800

    feat: Able to restrict 12 articles per page

--- pages/laLiga/fcbks-blog/index.tsx
+++ pages/laLiga/fcbks-blog/index.tsx
@@ -14,6 +14,8 @@ interface fCBkHomeProps {
 function Index({ articles }: fCBkHomeProps) {
   const [displayedArticles, setDisplayedArticles] = useState<articles[]>([]);
   const [currentPage, setCurrentPage] = useState(0);
+  const [startIndex, setStartIndex] = useState(0);
+  const [endIndex, setEndIndex] = useState(12);
   const tabs = useTabs();
   const setActiveTab = useSetActiveTab();
 
@@ -37,9 +39,13 @@ function Index({ articles }: fCBkHomeProps) {
         (article) => article.categoryName === selectedTab.name
       );
       setCurrentPage(0);
+      setStartIndex(0);
+      setEndIndex(12);
       setDisplayedArticles(filteredArticles);
     } else {
       setCurrentPage(0);
+      setStartIndex(0);
+      setEndIndex(12);
       setDisplayedArticles(articles);
     }
   };
@@ -61,7 +67,7 @@ function Index({ articles }: fCBkHomeProps) {
         </h2>
         <Tabs tabs={tabs} onClick={handleSelectedTab} />
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-3 md:gap-6 p-2 md:p-6">
-          {displayedArticles.map((article) => (
+          {displayedArticles.slice(startIndex, endIndex).map((article) => (
             <Link key={article._id} href={`/laLiga/fcbks-blog/${article.slug}`}>
               <div className="border border-transparent rounded-lg group cursor-pointer overflow-hidden">
                 <img
@@ -83,12 +89,13 @@ function Index({ articles }: fCBkHomeProps) {
                 <li>
                   {currentPage !== 0 ? (
                     <a
-                      className="relative block rounded-full bg-transparent px-3 py-1.5 text-sm text-black transition-all duration-300 hover:bg-black hover:text-yellow-300"
-                      href="#!"
+                      className="cursor-pointer relative block rounded-full bg-transparent px-3 py-1.5 text-sm text-black transition-all duration-300 hover:bg-black hover:text-yellow-300"
                       onClick={() => {
                         const previousPage = currentPage - 1;
                         if (previousPage >= 0) {
                           setCurrentPage(previousPage);
+                          setStartIndex(startIndex - 12);
+                          setEndIndex(endIndex - 12);
                         }
                       }}
                     >
@@ -101,19 +108,11 @@ function Index({ articles }: fCBkHomeProps) {
                 {numberOfPages.map((page, index) => (
                   <li key={index}>
                     {index === currentPage ? (
-                      <a
-                        className="relative block rounded-full px-3 py-1.5 text-sm transition-all duration-300 bg-black text-yellow-300"
-                        href="#!"
-                        onClick={() => setCurrentPage(index)}
-                      >
+                      <a className="relative block rounded-full px-3 py-1.5 text-sm transition-all duration-300 bg-black text-yellow-300">
                         {index}
                       </a>
                     ) : (
-                      <a
-                        className="relative block rounded-full bg-transparent px-3 py-1.5 text-sm text-black transition-all duration-300 hover:bg-black hover:text-yellow-300"
-                        href="#!"
-                        onClick={() => setCurrentPage(index)}
-                      >
+                      <a className="relative block rounded-full bg-transparent px-3 py-1.5 text-sm text-black transition-all duration-300">
                         {index}
                       </a>
                     )}
@@ -122,12 +121,13 @@ function Index({ articles }: fCBkHomeProps) {
                 <li>
                   {currentPage !== numberOfPages.length - 1 ? (
                     <a
-                      className="relative block rounded-full bg-transparent px-3 py-1.5 text-sm text-black transition-all duration-300 hover:bg-black hover:text-yellow-300"
-                      href="#!"
+                      className="cursor-pointer relative block rounded-full bg-transparent px-3 py-1.5 text-sm text-black transition-all duration-300 hover:bg-black hover:text-yellow-300"
                       onClick={() => {
                         const nextPage = currentPage + 1;
                         if (nextPage < numberOfPages.length) {
                           setCurrentPage(nextPage);
+                          setStartIndex(startIndex + 12);
+                          setEndIndex(endIndex + 12);
                         }
                       }}
                     >
